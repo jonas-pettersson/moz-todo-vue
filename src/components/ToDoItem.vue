75cc08c06cdb744d3952d41208d50a4f077bc221
Jonas <j.g.f.pettersson@gmail.com>
2021-02-03 22:25:09 +0100

    after refactor

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="checkbox" name="todo-item" :id="id" :checked="isDone" />
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: { required: true, type: String },
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+  },
+  data() {
+    return {
+      isDone: this.done,
+    };
+  },
+};
+</script>
